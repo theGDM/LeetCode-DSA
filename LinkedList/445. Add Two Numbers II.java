c4cb63d9292802edcda05c270830e79b9389a79f
theGDM <gyandeepmehra370@gmail.com>
2022-01-25 13:23:39 +0530

    Create 445. Add Two Numbers II.java

---
+++ LinkedList/445. Add Two Numbers II.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode ahead = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = ahead;
+        }
+        return prev;
+    }
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy, tail = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            int d1 = l1 == null ? 0 : l1.val;
+            int d2 = l2 == null ? 0 : l2.val;
+            ListNode temp = new ListNode();
+            temp.val = (d1 + d2 + carry) % 10;
+            carry = (d1 + d2 + carry) / 10;
+            tail.next = temp;
+            tail = temp;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        
+        return reverse(head.next);
+    }
+}
