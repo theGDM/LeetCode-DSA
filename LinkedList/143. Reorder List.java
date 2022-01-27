676504f6622ee77488a4d72dd1e445d5f70faa91
theGDM <gyandeepmehra370@gmail.com>
2022-01-27 16:40:15 +0530

    Create 143. Reorder List.java

---
+++ LinkedList/143. Reorder List.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode prev = null, curr = head;
+        while(curr != null){
+            ListNode ahead = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = ahead;
+        }
+        return prev;
+    }
+    
+    public ListNode middle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if(fast == null){
+            return prev;
+        }else{
+            return slow;
+        }
+    }
+    
+    public void reorderList(ListNode head) {
+        ListNode mid = middle(head);
+        ListNode second = reverse(mid.next);
+        mid.next = null;
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        
+        while(head != null || second != null){
+            if(head != null){
+                tail.next = head;
+                tail = head;
+                head = head.next;
+            }
+            
+            if(second != null){
+                tail.next = second;
+                tail = second;
+                second = second.next;
+            }
+        }
+        
+        tail.next = null;
+        head = dummy.next;
+    }
+}
