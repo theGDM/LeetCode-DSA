c8bf6dd3259c898c557979a906972d35809b1476
theGDM <gyandeepmehra370@gmail.com>
2022-03-25 08:41:00 +0530

    Create 1325. Delete Leaves With a Given Value.java

---
+++ Binary Tree/1325. Delete Leaves With a Given Value.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null){
+            return null;
+        }
+        
+        TreeNode left = removeLeafNodes(root.left, target);
+        TreeNode right = removeLeafNodes(root.right, target);
+        
+        //checking the current node is the target node + leaf node
+        if(left == null && right == null && root.val == target) {
+            return null;
+        }
+        
+        root.left = left;
+        root.right = right;
+        
+        return root;
+    }
+}
