81d507af956c279660f0844269fc0bc69f4451d1
theGDM <gyandeepmehra370@gmail.com>
2022-04-04 22:08:29 +0530

    Update 1337. The K Weakest Rows in a Matrix (27-03-22).java

--- Daily Challenge/1337. The K Weakest Rows in a Matrix (27-03-22).java
+++ Daily Challenge/1337. The K Weakest Rows in a Matrix (27-03-22).java
@@ -92,3 +92,40 @@ class Solution {
         return res;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        for(int i = 0;i < k - 1; ++i){
+            fast = fast.next;
+        }
+        
+        ListNode first = fast;
+        
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        ListNode last = slow;
+        
+        int temp = first.val;
+        first.val = last.val;
+        last.val = temp;
+            
+        return head;
+    }
+}
