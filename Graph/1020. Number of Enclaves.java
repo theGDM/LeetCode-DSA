5bb1869b56d4d1f479dc141dd301f684ca96083b
theGDM <gyandeepmehra370@gmail.com>
2022-06-14 17:12:40 +0530

    Update 1020. Number of Enclaves.java

--- Graph/1020. Number of Enclaves.java
+++ Graph/1020. Number of Enclaves.java
@@ -96,3 +96,93 @@ class Solution {
         landAreaNotTouchingWall(r - 1, c, grid, visited); //up
     }
 }
+
+//in the 3rd approach we will first mark lands to water, while traversing over the boundry of grid, so we will get rid of the 
+//all the land area that touching the boundaries and the we simply again iterate over each cell not belonging to boundary and
+//count all the cells of land
+
+class Solution {
+    int count = 0;
+    public int numEnclaves(int[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        
+        //we will traverse over boundery and remove the continuous land area, tha touching boundaries
+        markBoundryLand(grid, visited);
+        
+        //now traverse again inside the boundary
+        for(int r = 1;r <= grid.length - 2; ++r){
+            for(int c = 1;c <= grid[0].length - 2; ++c){
+                if(grid[r][c] == 1 && visited[r][c] == false){
+                    landAreaNotTouchingWall(r, c, grid, visited);
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    public void markBoundryLand(int[][] grid, boolean[][] visited){
+        int frow = 0, lrow = grid.length - 1;
+        int fcol = 0, lcol = grid[0].length - 1;
+        
+        for(int i = fcol; i <= lcol; ++i){
+            markLandAreaTouchingWall(frow, i, grid, visited);
+        }
+        ++frow;
+        
+        for(int j = frow; j <= lrow; ++j){
+            markLandAreaTouchingWall(j, lcol, grid, visited);
+        }
+        --lcol;
+        
+        for(int k = lcol; k >= fcol; --k){
+            markLandAreaTouchingWall(lrow, k, grid, visited);
+        }
+        --lrow;
+        
+        for(int l = lrow; l >= frow; --l){
+            markLandAreaTouchingWall(l, fcol, grid, visited);
+        }
+        ++fcol;
+    }
+    
+    public void markLandAreaTouchingWall(int r, int c, int[][] grid, boolean[][] visited){
+        //base case
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length){
+            return; 
+        }else if(visited[r][c] == true){
+            return; //this cell is already marked
+        }else if(grid[r][c] == 0){
+            return; //water
+        } 
+        
+        //preorder work
+        visited[r][c] = true;
+        grid[r][c] = 0;
+        //faith calls
+        markLandAreaTouchingWall(r, c - 1, grid, visited); //left
+        markLandAreaTouchingWall(r + 1, c, grid, visited); //down
+        markLandAreaTouchingWall(r, c + 1, grid, visited); //right
+        markLandAreaTouchingWall(r - 1, c, grid, visited); //up
+    }
+    
+    public void landAreaNotTouchingWall(int r, int c, int[][] grid, boolean[][] visited){
+        //base case
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length){
+            return; 
+        }else if(visited[r][c] == true){
+            return; //this cell is already marked
+        }else if(grid[r][c] == 0){
+            return; //water
+        } 
+        
+        //preorder work
+        visited[r][c] = true;
+        ++count;
+        //faith calls
+        landAreaNotTouchingWall(r, c - 1, grid, visited); //left
+        landAreaNotTouchingWall(r + 1, c, grid, visited); //down
+        landAreaNotTouchingWall(r, c + 1, grid, visited); //right
+        landAreaNotTouchingWall(r - 1, c, grid, visited); //up
+    }
+}
