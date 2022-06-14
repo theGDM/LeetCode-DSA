f49bd9e66f09ae3049f17bfcc297ca423ced4001
theGDM <gyandeepmehra370@gmail.com>
2022-06-14 17:17:34 +0530

    Create 994. Rotting Oranges.java

---
+++ Graph/994. Rotting Oranges.java
@@ -0,0 +1,73 @@
+class Solution {
+    int freshCount = 0;
+    public int orangesRotting(int[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        
+        Queue<Pair> q = new ArrayDeque<>();
+        for(int r = 0;r < grid.length; ++r){
+            for(int c = 0;c < grid[0].length; ++c){
+                if(grid[r][c] == 2){
+                    q.add(new Pair(r, c, 0)); //add all the rotten oranges in the queue
+                }else{
+                    ++freshCount; //count fresh oranges
+                }
+            }
+        }
+        
+        infectOranges(grid, visited, q);
+        
+        if(freshCount == 0){
+            return time;
+        }else{
+           return -1;  
+        } 
+    }
+    
+    public class Pair{
+        int r;
+        int c;
+        int rt;
+        
+        Pair(int r, int c, int rt){
+            this.r = r; //current row
+            this.c = c; //current col
+            this.rt = rt; //rottening time
+        }
+    }
+    
+    int time = 0;
+    public void infectOranges(int[][] grid, boolean[][] visited, Queue<Pair> q){
+        while(q.size() > 0){
+            Pair rp = q.remove(); //remove
+            
+            if(visited[rp.r][rp.c] == true){ //mark*
+                continue;
+            }
+            
+            visited[rp.r][rp.c] = true; 
+            
+            //work
+            if(rp.rt > time){
+                time = rp.rt;
+            }
+            
+            if(grid[rp.r][rp.c] == 1){
+                --freshCount; //decrease the count of the fresh oranges by one
+            }
+            
+            //add nbrs
+            addNbrs(rp.r, rp.c - 1, rp.rt + 1, visited, grid, q);
+            addNbrs(rp.r + 1, rp.c, rp.rt + 1, visited, grid, q);
+            addNbrs(rp.r, rp.c + 1, rp.rt + 1, visited, grid, q);
+            addNbrs(rp.r - 1, rp.c, rp.rt + 1, visited, grid, q); 
+        }
+    }
+    
+    public void addNbrs(int r, int c, int rt, boolean[][] visited, int[][] grid, Queue<Pair> q){
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || visited[r][c] == true || grid[r][c] == 0){
+            return;
+        }
+        
+        q.add(new Pair(r, c, rt));
+    }
+}
