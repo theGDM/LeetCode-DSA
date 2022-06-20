e18e8cfa8de7975ce08d3711193a502789cff8d6
theGDM <gyandeepmehra370@gmail.com>
2022-06-20 21:24:47 +0530

    Update 797. All Paths From Source to Target.java

--- Graph/797. All Paths From Source to Target.java
+++ Graph/797. All Paths From Source to Target.java
@@ -1,3 +1,4 @@
+//1st way, node ke pre and post area me add and remove
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
@@ -29,5 +30,39 @@ class Solution {
         //postorder work
         visited[src] = false;
         path.remove(path.size() - 1);
+    }
+}
+
+//2nd way /edge ke preorder and postorder me add and remove resp.
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        boolean[] visited = new boolean[graph.length];
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        getAllPath(0, graph.length - 1, graph, visited, path);
+        return res;
+    }
+    
+    public void getAllPath(int src, int dest, int[][] graph, boolean[] visited, List<Integer> path){
+        if(src == dest){
+            res.add(new ArrayList(path));
+            return;
+        }
+        
+        //preorder work
+        visited[src] = true;
+        
+        //add nbrs
+        for(int v : graph[src]){
+            if(visited[v] == false){
+                path.add(v);
+                getAllPath(v, dest, graph, visited, path);
+                path.remove(path.size() - 1);
+            }
+        }
+        
+        //postorder work
+        visited[src] = false;
     }
 }
