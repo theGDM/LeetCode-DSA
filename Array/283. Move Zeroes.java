b3545db0bd18ec7eda282782941dd27435e89172
theGDM <gyandeepmehra370@gmail.com>
2022-06-30 17:59:13 +0530

    Create 283. Move Zeroes.java

---
+++ Array/283. Move Zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; ++i){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                ++j;
+            }
+        }
+        
+        while(j < nums.length){
+            nums[j] = 0;
+            ++j;
+        }
+    }
+}
