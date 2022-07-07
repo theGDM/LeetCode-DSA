684b3e4b8c034596885065d762af57fbc4035b2a
theGDM <gyandeepmehra370@gmail.com>
2022-07-07 09:05:15 +0530

    Update 1550. Three Consecutive Odds.java

--- Math/1550. Three Consecutive Odds.java
+++ Math/1550. Three Consecutive Odds.java
@@ -1,3 +1,4 @@
+//1st approach
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         if(arr.length < 3){
@@ -7,6 +8,22 @@ class Solution {
         for(int i = 0;i < arr.length - 2; ++i){
             if(arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1){
                 return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+//2nd approach
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for(int i = 0;i < arr.length; ++i){
+            if(arr[i] % 2 == 0){
+                count = 0;
+            }else{
+                if(++count == 3) return true;
             }
         }
         
