adab2962c148132dda127100422df1e2eba3893d
theGDM <gyandeepmehra370@gmail.com>
2022-07-07 09:23:14 +0530

    Update 2181. Merge Nodes in Between Zeros.java

--- LinkedList/2181. Merge Nodes in Between Zeros.java
+++ LinkedList/2181. Merge Nodes in Between Zeros.java
@@ -32,3 +32,39 @@ class Solution {
         return dummyHead.next;
     }
 }
+
+//2nd Approach, with sc = O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode alternateTrav = head.next; //alternate traversal to count the sum inbetween nodes with 0 val
+        ListNode mainTrav = head; //mainTrav to keep res track
+        
+        int currSum = 0;
+        while(alternateTrav != null){
+            if(alternateTrav.val == 0){
+                mainTrav.val = currSum;
+                if(alternateTrav.next != null){
+                    mainTrav.next = alternateTrav;
+                    mainTrav = mainTrav.next;
+                }
+                currSum = 0;
+            }
+            
+            currSum += alternateTrav.val;
+            alternateTrav = alternateTrav.next;
+        }
+        
+        mainTrav.next = null;
+        return head;
+    }
+}
