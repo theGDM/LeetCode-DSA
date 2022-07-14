6bb8e7e22cd488f816f4a1c998ad02fb5c0cb51d
theGDM <gyandeepmehra370@gmail.com>
2022-07-14 13:42:46 +0530

    Create 263. Ugly Number.java

---
+++ Math/263. Ugly Number.java
@@ -0,0 +1,16 @@
+//TC : O(n)
+class Solution {
+    public boolean isUgly(int n) {
+        if (n == 0) return false;
+        if (n == 1) return true;
+        
+        while(n != 1){
+            if(n % 2 == 0) n = n / 2;
+            else if(n % 3 == 0) n = n / 3;
+            else if(n % 5 == 0) n = n / 5;
+            else return false;
+        }
+        
+        return true;
+    }
+}
