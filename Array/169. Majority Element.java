7678315d8ea093fe4ebc6b1358cce66d371d70c1
theGDM <gyandeepmehra370@gmail.com>
2022-07-16 00:22:52 +0530

    Update 169. Majority Element.java

--- Array/169. Majority Element.java
+++ Array/169. Majority Element.java
@@ -25,4 +25,22 @@ class Solution {
         
         return major;
     }
+    
+    //approch 3
+    public int majorityElement(int[] nums) {
+        int major = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; ++i){
+            if(count == 0){
+                major = nums[i];
+                count++;
+            }else if(nums[i] == major){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        
+        return major;
+    }
 }
