707e4c4d86ccea47c14666e27a5350e4d78c340b
theGDM <gyandeepmehra370@gmail.com>
2022-07-15 17:12:01 +0530

    Update 456. 132 Pattern.java

--- Stack & Queue/456. 132 Pattern.java
+++ Stack & Queue/456. 132 Pattern.java
@@ -37,4 +37,32 @@ class Solution {
     }
 }
 
-
+//O(n) solution
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int[] min = new int[nums.length + 1];
+        
+        min[0] = nums[0];
+        for(int i = 1; i < nums.length; ++i){
+            min[i] = Math.min(nums[i], min[i - 1]);
+        }
+        
+        Stack<Integer> stk = new Stack<>();
+        
+        for(int j = nums.length - 1; j >= 0; --j){
+            while(stk.size() > 0 && stk.peek() <= min[j]){
+                stk.pop();
+            }
+            
+            if(stk.size() > 0){
+                if(stk.peek() < nums[j]){
+                    return true;
+                }
+            }
+            
+            stk.push(nums[j]);
+        }
+        
+        return false;
+    }
+}
