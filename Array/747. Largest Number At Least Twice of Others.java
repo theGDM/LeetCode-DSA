d55eec522e3c081deefb1be452a7c722ed070c99
theGDM <gyandeepmehra370@gmail.com>
2022-07-19 00:38:29 +0530

    Update 747. Largest Number At Least Twice of Others.java

--- Array/747. Largest Number At Least Twice of Others.java
+++ Array/747. Largest Number At Least Twice of Others.java
@@ -23,3 +23,27 @@ class Solution {
         return maxIdx;
     }
 }
+
+//1 traversal algorithm
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int fmi = -1;
+        int smi = -1;
+        
+        //finding the max
+        for(int i = 0; i < nums.length; ++i){
+            if(fmi == -1 || nums[i] >= nums[fmi]){
+                smi = fmi;
+                fmi = i;
+            }else if(smi == -1 || nums[i] >= nums[smi]){
+                smi = i;
+            }
+        }
+    
+        if(nums[smi] * 2 > nums[fmi]){
+            return -1;
+        }else{
+           return fmi; 
+        }
+    }
+}
