e2e3b567e8133d3688d9d01b39f497bd08756cfa
theGDM <gyandeepmehra370@gmail.com>
2022-07-19 17:58:26 +0530

    Update 1315. Sum of Nodes with Even-Valued Grandparent.java

--- Binary Tree/1315. Sum of Nodes with Even-Valued Grandparent.java
+++ Binary Tree/1315. Sum of Nodes with Even-Valued Grandparent.java
@@ -37,3 +37,57 @@ class Solution {
         helper(r.right, r, p, level + 1); 
     }
 }
+
+//BFS : O(n)
+class Solution {
+    public class Pair{
+        TreeNode n;
+        TreeNode p;
+        TreeNode gp;
+        
+        Pair(TreeNode n, TreeNode p, TreeNode gp){
+            this.n = n;
+            this.p = p;
+            this.gp = gp;
+        }
+    }
+    
+    public int sumEvenGrandparent(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(root, null, null));
+        
+        int level = 1;
+        int sum = 0;
+        
+        while(q.size() > 0){
+            int size = q.size();
+            
+            while(size-- > 0){
+                //remove
+                Pair curr = q.remove();
+                
+                //work
+                if(level >= 3 && curr.gp.val % 2 == 0){
+                    sum += curr.n.val;
+                }
+                
+                //add childs
+                if(curr.n.left != null){
+                    q.add(new Pair(curr.n.left, curr.n, curr.p));
+                }
+                
+                if(curr.n.right != null){
+                    q.add(new Pair(curr.n.right, curr.n, curr.p));
+                }
+            }
+            
+            level++;
+        }
+        
+        return sum;
+    }
+}
