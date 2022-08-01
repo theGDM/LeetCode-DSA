8da13000c4bc33ac8dafdde2ff469e10232ed45d
theGDM <gyandeepmehra370@gmail.com>
2022-08-01 22:57:20 +0530

    Create 1768. Merge Strings Alternately.java

---
+++ String & StringBuilder/1768. Merge Strings Alternately.java
@@ -0,0 +1,27 @@
+//O(n) solution
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0;
+        int j = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(i < word1.length() && j < word2.length()){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+            
+            i++;
+            j++;
+        }
+        
+        if(i < word1.length()){
+            String rem = word1.substring(i);
+            sb.append(rem);
+        }else if(j < word2.length()){
+            String rem = word2.substring(j);
+            sb.append(rem);
+        }
+        
+        return sb.toString();
+    }
+}
