18455fe4724860331215d67b21774a912c658528
theGDM <gyandeepmehra370@gmail.com>
2022-08-03 00:29:32 +0530

    Update 929. Unique Email Addresses.java

--- HashMap and Heap/929. Unique Email Addresses.java
+++ HashMap and Heap/929. Unique Email Addresses.java
@@ -31,3 +31,44 @@ class Solution {
         return hm.keySet().size();
     } 
 }
+
+//O(n * emails[i].length) ~ O(n * 100) ~ O(n)
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        if(emails.length == 0) return 0;
+        
+        HashSet<String> set = new HashSet<>();
+        for(String email : emails){
+            StringBuilder sb = new StringBuilder();
+            
+            //making clean local part
+            int i = 0;
+            while(i < email.length()){
+                char ch = email.charAt(i);
+                if(ch == '.'){
+                    i++;
+                    continue;
+                }else if(ch == '+' || ch == '@'){
+                    break;
+                }else{
+                   sb.append(ch); 
+                }
+                i++;
+            }
+            
+            //adding domain part
+            while(i < email.length()){
+                char ch = email.charAt(i);{
+                    if(ch == '@'){
+                        sb.append(email.substring(i));
+                    }
+                }
+                
+                i++;
+            }
+            set.add(sb.toString());
+        }
+        
+        return set.size();
+    } 
+}
