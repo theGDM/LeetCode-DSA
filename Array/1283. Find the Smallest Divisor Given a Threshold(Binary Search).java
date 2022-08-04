71cb3d80913b07dd41b68754f52d874a987d4aa0
theGDM <gyandeepmehra370@gmail.com>
2022-08-04 23:57:38 +0530

    Update 1283. Find the Smallest Divisor Given a Threshold(Binary Search).java

--- Array/1283. Find the Smallest Divisor Given a Threshold(Binary Search).java
+++ Array/1283. Find the Smallest Divisor Given a Threshold(Binary Search).java
@@ -2,31 +2,6 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
         int lo = 1;
-        int hi = 1000000;
-        
-        while(lo < hi){
-            int mid = lo + (hi - lo) / 2;
-            
-            int sum = 0;
-            for(int val : nums){
-                int q = (val + mid - 1) / mid;
-                sum += q;
-            }
-            
-            if(sum > threshold){
-                lo = mid + 1; //kyuki mid per gadbad aaya hi hai to, to lo ko mid + 1 kar do
-            }else{
-                hi = mid;
-            }
-        }
-        //bahar nikalte hi lo aur hi same hi honge!
-        return lo; 
-    }
-}
-
-class Solution {
-    public int smallestDivisor(int[] nums, int threshold) {
-        int lo = 1;
         int hi = Integer.MIN_VALUE;
       
         for(int val : nums){
@@ -40,17 +15,17 @@ class Solution {
             
             int sum = 0;
             for(int val : nums){
-                int q = (val + mid - 1) / mid;
+                int q = (val + mid - 1) / mid; //add (mid - 1) to get ceil of quotient
                 sum += q;
             }
             
             if(sum > threshold){
                 lo = mid + 1; //kyuki mid per gadbad aaya hi hai to, to lo ko mid + 1 kar do
             }else{
-                hi = mid;
+                hi = mid; //matlab aur accha answer nikalo ,isse
             }
         }
-        //bahar nikalte hi lo aur hi same hi honge!
-        return lo; 
+        
+        return lo;
     }
 }
