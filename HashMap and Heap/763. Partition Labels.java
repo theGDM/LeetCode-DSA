9ded12e05fda0a3770f994836858145d9a934463
theGDM <gyandeepmehra370@gmail.com>
2022-08-04 16:10:25 +0530

    Update 763. Partition Labels.java

--- HashMap and Heap/763. Partition Labels.java
+++ HashMap and Heap/763. Partition Labels.java
@@ -29,3 +29,36 @@ class Solution {
         return res;
     }
 }
+
+
+//Using Array..TC : O(n)
+//SC : O(n)
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        int[] map = new int[26];
+        Arrays.fill(map, -1);
+        
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            map[ch - 'a'] = i;
+        }
+        
+        
+        int st = 0;
+        int en = 0;
+        
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            en = Math.max(en, map[ch - 'a']);
+            
+            if(i == en) {
+                int resLen = en - st + 1;
+                res.add(resLen);
+                en = st = i + 1;
+            }
+        }
+        
+        return res;
+    }
+}
