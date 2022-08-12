3c6e5a21614dbc88913c6ef0b61abccccd98a423
theGDM <gyandeepmehra370@gmail.com>
2022-08-12 11:44:10 +0530

    Update 2006. Count Number of Pairs With Absolute Difference K.java

--- Two Pointers/2006. Count Number of Pairs With Absolute Difference K.java
+++ Two Pointers/2006. Count Number of Pairs With Absolute Difference K.java
@@ -21,3 +21,29 @@ class Solution {
         return ans;
     }
 }
+
+
+// [7,7,8,3,1,2,7,2,9,5]
+// 6
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        int count = 0;
+        for(int key : map.keySet()){
+            int f1 = map.get(key);
+            
+            if(k == 0){ //never reach at this statement, as k >= 1
+                count += f1 * (f1 - 1) / 2;
+            }else{
+                int f2 = map.getOrDefault(key + k, 0);
+                count += f1 * f2;
+            }
+        }
+        
+        return count;
+    }
+}
