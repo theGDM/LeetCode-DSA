5d90eb4871d44b298bb867d6460a85caa051743d
theGDM <gyandeepmehra370@gmail.com>
2022-08-16 15:18:21 +0530

    Create 98. Validate Binary Search Tree.java

---
+++ Binary Search Tree/98. Validate Binary Search Tree.java
@@ -0,0 +1,88 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//O(n) approach
+class Solution {
+    public void getInorder(TreeNode node, ArrayList<Integer> in){
+        if(node == null){
+          return;
+        }
+
+        getInorder(node.left, in);//pre area
+        in.add(node.val);//in area
+        getInorder(node.right, in);//post area
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> inorder = new ArrayList<>(); 
+        getInorder(root, inorder);
+
+        for(int i = 0;i < inorder.size() - 1; ++i){
+            if(inorder.get(i) >= inorder.get(i + 1)){
+              return false;
+            }
+        }
+
+       return true;
+    }
+}
+
+//TC : O(n2) Approach
+class Solution {
+  public Long max(TreeNode node){
+    if(node == null){
+      return Long.MIN_VALUE;
+    }
+
+    long lmax = max(node.left);
+    long rmax = max(node.right);
+
+    return Math.max(rmax, Math.max(lmax, node.val));
+  }
+
+  public Long min(TreeNode node){
+    if(node == null){
+      return Long.MAX_VALUE;
+    }
+
+    long lmin = min(node.left);
+    long rmin = min(node.right);
+
+    return Math.min(rmin, Math.min(lmin, node.val));
+  }
+
+  public boolean isValidBST(TreeNode root){
+   if(root == null){
+     return true;
+   }
+   
+   if(root.left == null && root.right == null){
+       return true;
+   }
+      
+   long maxOfLeftSubTree = max(root.left);//getting max from left sub tree of current node
+   long minOfRightSubTree = min(root.right);//getting min from right sub tree of current node
+
+   boolean ltans = isValidBST(root.left);//getting ans from left sub tree of current node whether it is BST or not.
+   boolean rtans = isValidBST(root.right);//getting ans from right sub tree of current node whether it is BST or not.
+
+   //if currnt node + its left subtree + its right subtree are BST then return true otherwise false
+   if((long)root.val > maxOfLeftSubTree && (long)root.val < minOfRightSubTree && ltans && rtans){
+     return true;
+   }
+   return false;
+  }
+}
