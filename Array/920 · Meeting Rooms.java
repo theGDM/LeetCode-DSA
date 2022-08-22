5f862a8a54ef17c86fbf10d783f547e48f113641
theGDM <gyandeepmehra370@gmail.com>
2022-08-22 18:44:57 +0530

    Update and rename 919 · Meeting Rooms.java to 920 · Meeting Rooms.java

--- Array/920 · Meeting Rooms.java
+++ Array/920 · Meeting Rooms.java
@@ -52,3 +52,51 @@ public class Solution {
         return true;
     }
 }
+
+
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+//Using comparator
+public class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: if a person could attend all meetings
+     */
+
+    // Helper class implementing Comparator interface
+    public static class sortByStart implements Comparator<Interval>{
+        public int compare(Interval a, Interval b){
+            return a.start - b.start;
+        }
+    }
+
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        // Write your code here
+        if(intervals.size() == 0 || intervals == null) return true;
+        
+        PriorityQueue<Interval> pq = new PriorityQueue<>(new sortByStart());
+        for(Interval p : intervals){
+            pq.add(p);
+        }
+
+        int currEnd = pq.remove().end;
+        while(pq.size() > 0){ 
+            Interval rem = pq.remove();
+            if(rem.start < currEnd){
+                return false;
+            }else{
+                currEnd = rem.end;
+            }
+        }
+
+        return true;
+    }
+}
