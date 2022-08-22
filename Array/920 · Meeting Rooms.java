f8166f68f5017bb72ebe18956fc714f102d1399f
theGDM <gyandeepmehra370@gmail.com>
2022-08-22 18:49:05 +0530

    Update 920 · Meeting Rooms.java

--- Array/920 · Meeting Rooms.java
+++ Array/920 · Meeting Rooms.java
@@ -100,3 +100,41 @@ public class Solution {
         return true;
     }
 }
+
+
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+//TC : nlog(n)
+//SC : O(n)
+public class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: if a person could attend all meetings
+     */
+
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        // Write your code here
+        if(intervals.size() == 0 || intervals == null) return true;
+
+        //nlog(n)
+        Collections.sort(intervals, (a, b) -> {
+            return a.start - b.start;
+        });
+
+        for(int i = 0; i < intervals.size() - 1; i++){
+            if(intervals.get(i).end > intervals.get(i + 1).start){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
