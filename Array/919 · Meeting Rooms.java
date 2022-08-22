4748724bc6f116561ff5a962a0ff02748131135e
theGDM <gyandeepmehra370@gmail.com>
2022-08-22 18:38:23 +0530

    Create 919 · Meeting Rooms.java

---
+++ Array/919 · Meeting Rooms.java
@@ -0,0 +1,54 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+//TC : O(nlog(n))
+//SC : O(n);
+public class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: if a person could attend all meetings
+     */
+
+    public class Pair implements Comparable<Pair>{
+        int start;
+        int end;
+
+        Pair(int start, int end){
+            this.start = start;
+            this.end = end;
+        }
+
+        public int compareTo(Pair o){
+            return this.start - o.start;
+        }
+    }
+
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        // Write your code here
+        if(intervals.size() == 0) return true;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(Interval p : intervals){
+            pq.add(new Pair(p.start, p.end));
+        }
+
+        int currEnd = pq.remove().end;
+        while(pq.size() > 0){
+            Pair rem = pq.remove();
+            if(rem.start < currEnd){
+                return false;
+            }else{
+                currEnd = rem.end;
+            }
+        }
+
+        return true;
+    }
+}
