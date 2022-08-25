43fe988573adf8fb31b0560b0cf4dc23158c090f
theGDM <gyandeepmehra370@gmail.com>
2022-08-25 22:05:34 +0530

    Update 300. Longest Increasing Subsequence.java

--- Dynamic Programming/300. Longest Increasing Subsequence.java
+++ Dynamic Programming/300. Longest Increasing Subsequence.java
@@ -27,3 +27,30 @@ class Solution {
         return res;
     }
 }
+
+//TC : nlog(n)
+//Har length ki Longest increasing subsequence ka, last value store karke rakh rahe hai
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int len = 1;
+        tails[0] = nums[0];
+        
+        for(int i = 1; i < nums.length; ++i){
+            if(nums[i] > tails[len - 1]){
+                len++; //increase the length, and place the value
+                tails[len - 1] = nums[i];
+            }else{
+                int idx = Arrays.binarySearch(tails, 0, len - 1, nums[i]); //it will give the position if
+                if(idx < 0){ //that element found, otherwise, return the -ve indx, at which positon it 
+                    idx = -idx; //should be placed..
+                    idx = idx - 1;
+                }
+                
+                tails[idx] = nums[i];
+            }
+        }
+        
+        return len;
+    }
+}
