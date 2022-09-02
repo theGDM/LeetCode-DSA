bf46345f591fdc5eba5111e8ec8c9f0ab745d235
theGDM <gyandeepmehra370@gmail.com>
2022-09-02 16:34:47 +0530

    Create Flattening a Linked List.java

---
+++ LinkedList/Flattening a Linked List.java
@@ -0,0 +1,31 @@
+class GfG{
+    Node flatten(Node root){
+	    if(root == null || root.next == null){ //0 or 1 node return it
+	        return root;
+	    }
+	    
+	    Node b = flatten(root.next);
+	    return merge2SortedList(root, b); //postorder me merge kara do
+    }
+    
+    Node merge2SortedList(Node first, Node second){
+        Node head = new Node(-1);
+        Node tail = head;
+        
+        while(first != null && second != null){
+            if(first.data <= second.data){
+                tail.bottom = first;
+                first = first.bottom;
+                tail = tail.bottom;
+            }else{
+                tail.bottom = second;
+                second = second.bottom;
+                tail = tail.bottom;
+            }
+        }
+        
+        if(first == null) tail.bottom = second;
+        else tail.bottom = first;
+        return head.bottom;
+    }
+}
