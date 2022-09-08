b593eb0ac070c8766cc70b6e1aaeacd915285d48
theGDM <gyandeepmehra370@gmail.com>
2022-09-08 18:19:48 +0530

    Update 962. Maximum Width Ramp.java

--- Two Pointers/962. Maximum Width Ramp.java
+++ Two Pointers/962. Maximum Width Ramp.java
@@ -53,3 +53,31 @@ class Solution {
         return ans;
     }
 }
+
+
+//Classical two pointer problem. j pointer expands the range and i pointer contracts it. The trick is that i pointer iterates over original array and j pointer iterates over an array which stores maximum no. on the right for each index.
+//O(n)
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int[] maxr = new int[nums.length]; //suffix max array
+        
+        maxr[nums.length - 1] = nums[nums.length - 1];
+        for(int i = nums.length - 2; i >= 0; --i){
+            maxr[i] = Math.max(maxr[i + 1], nums[i]);
+        }
+        
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        while(i < nums.length && j < nums.length){
+            if(nums[i] <= maxr[j]){ //jaha tak ja sakte ho chale jao, means move j pointer
+                ans = Math.max(ans, j - i);
+                j++;
+            }else{
+                i++; 
+            }
+        }
+        
+        return ans;
+    }
+}
