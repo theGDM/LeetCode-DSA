5548f633a966a754ed77c447ebfcf84b1c7fd0ac
theGDM <gyandeepmehra370@gmail.com>
2022-09-09 16:35:58 +0530

    Create 1004. Max Consecutive Ones III.java

---
+++ Two Pointers/1004. Max Consecutive Ones III.java
@@ -0,0 +1,33 @@
+//Acuired and release startegy
+//TC : O(n) //SC : O(1)
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int i = 0; //include pointer
+        int j = 0; //exclude pointer
+        
+        int cz = 0; //count of zeros
+        int res = 0; //result
+        
+        while(i < nums.length){
+            if(nums[i] == 0){
+                cz++;
+            }
+            
+            while(cz > k){
+                if(nums[j] == 0){
+                    cz--;
+                }
+                
+                j++;
+            }
+            
+            int len = i - j + 1; //calculate length
+            // System.out.println(i + " - " + j + " => " + len);
+            res = Math.max(res, len);
+            i++; 
+        }
+        
+        return res;
+    }
+        
+}
