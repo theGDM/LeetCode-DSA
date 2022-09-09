476e2cb792ac82b1857e84ca25ec3eb5b2c7d258
theGDM <gyandeepmehra370@gmail.com>
2022-09-09 16:38:20 +0530

    Create 883 · Max Consecutive Ones II.java

---
+++ Two Pointers/883 · Max Consecutive Ones II.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param nums: a list of integer
+     * @return: return a integer, denote  the maximum number of consecutive 1s
+     */
+
+    //acuired realese startegy 
+    //TC : O(n)
+    //SC : O(1)
+    public int findMaxConsecutiveOnes(int[] nums) {
+        // write your code here
+        int cz = 0;
+        int i = 0; //acquire pointer
+        int j = 0; //release pointer
+        int ans = 0; //final result
+        while(i < nums.length){
+            if(nums[i] == 0){
+                cz++;
+            }
+            
+            //if number of 0's are greater than threshold, make it valid by moving j pointer
+            while(cz > 1){
+                if(nums[j] == 0){
+                    cz--;
+                }
+                j++;
+            }
+
+            int len = i - j + 1;
+            ans = Math.max(ans, len);
+            i++;
+        }
+
+        return ans;
+    }
+}
