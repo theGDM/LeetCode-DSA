30cd249fd1a363a60666627f4596bc7dc56111d0
theGDM <gyandeepmehra370@gmail.com>
2022-09-11 15:02:15 +0530

    Update Maximum sum of subarray less than or equal to x.java

--- Two Pointers/Maximum sum of subarray less than or equal to x.java
+++ Two Pointers/Maximum sum of subarray less than or equal to x.java
@@ -1,8 +1,8 @@
 //TC : O(n)
 class Solution{
     long findMaxSubarraySum(long nums[], int N,int X){
-        int l = 0, maxSum = Integer.MIN_VALUE;
-        int sum = 0;
+        int l = 0;
+        long maxSum = Long.MIN_VALUE, sum = 0;
         for(int r = 0; r < nums.length; ++r){
             sum += nums[r];
             
@@ -14,7 +14,7 @@ class Solution{
             maxSum = Math.max(maxSum, sum);
         }
         
-        if(maxSum == Integer.MIN_VALUE) return 0;
+        if(maxSum == Long.MIN_VALUE) return 0;
         return maxSum;
     }
 }
