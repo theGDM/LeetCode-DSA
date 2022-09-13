02dc9360663225c10e493aae609efcf5c796cd0e
theGDM <gyandeepmehra370@gmail.com>
2022-09-13 21:22:41 +0530

    Update Maximum Sum Triplet.java

--- Two Pointers/Maximum Sum Triplet.java
+++ Two Pointers/Maximum Sum Triplet.java
@@ -28,3 +28,32 @@ public class Solution {
         return ans;
     }
 }
+
+
+public class Solution {
+    //TC : O(nlog(n))
+    public int solve(ArrayList<Integer> A) {
+        int ans = Integer.MIN_VALUE;
+        int[] maxRight = new int[A.size()]; //suffix max array
+        maxRight[A.size() - 1] = A.get(A.size() - 1);
+        for(int i = A.size() - 2; i >= 0; --i){
+            maxRight[i] = Math.max(maxRight[i + 1], A.get(i));
+        }
+        
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(A.get(0));
+        for(int i = 1; i < A.size() - 1; ++i){
+            int mid = A.get(i); //getting the mid element
+            int rMax = maxRight[i + 1]; //getting the max element in range [i + 1  to n]
+            if(mid < rMax){
+                Integer lMax = ts.lower(mid); //getting the max in O(logn) in range [0 to i - 1], just smaller than mid
+                if(lMax != null){
+                    ans = Math.max(ans, lMax + mid + rMax);
+                }
+            }
+            ts.add(mid); //add in tree set
+        }
+        
+        return ans;
+    }
+}
