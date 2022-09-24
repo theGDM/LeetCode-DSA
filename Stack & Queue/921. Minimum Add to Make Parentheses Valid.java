819ec76a705deb10801e2e04e8cf4e61bb3b9f01
theGDM <gyandeepmehra370@gmail.com>
2022-09-24 12:04:43 +0530

    Create 921. Minimum Add to Make Parentheses Valid.java

---
+++ Stack & Queue/921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    //TC : O(n)
+    //SC : O(1)
+    public int minAddToMakeValid(String s) {
+        int open = 0, close = 0;
+        
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                open++;
+            }else{
+                if(open == 0) close++;
+                else open--;
+            }
+        }
+        
+        return open + close;
+    }
+}
