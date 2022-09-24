821eec9bfda238476c6bbf7f91dc92269fad0ce4
theGDM <gyandeepmehra370@gmail.com>
2022-09-24 21:40:00 +0530

    Update 1021. Remove Outermost Parentheses.java

--- Stack & Queue/1021. Remove Outermost Parentheses.java
+++ Stack & Queue/1021. Remove Outermost Parentheses.java
@@ -47,3 +47,26 @@ class Solution {
         return res.toString();
     }
 }
+
+
+//TC : O(n)
+//SC : O(1)
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count = 0;
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                if(count > 0) res.append(ch); //first add then count++
+                count++;
+            }else{
+                count--; //first count--, then add
+                if(count > 0) res.append(ch);
+            }
+        }
+        
+        return res.toString();
+    }
+}
