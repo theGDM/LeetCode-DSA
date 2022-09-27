e6c668e82132aa3a0feb8d6c12867980c0b9a3ab
theGDM <gyandeepmehra370@gmail.com>
2022-09-27 23:31:22 +0530

    Create 1190. Reverse Substrings Between Each Pair of Parentheses.java

---
+++ Stack & Queue/1190. Reverse Substrings Between Each Pair of Parentheses.java
@@ -0,0 +1,40 @@
+//"a(bcdefghijkl(mno)p)q"
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<String> stk = new Stack<>();
+        
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                stk.push("(");
+            }else if(ch >= 'a' && ch <= 'z'){
+                if(stk.size() > 0 && stk.peek() != "("){
+                    String str = stk.pop() + ch;
+                    stk.push(str);
+                }else{
+                    stk.push(ch + "");
+                }   
+            }else{
+                StringBuilder part = new StringBuilder("");
+                while(stk.size() > 0 && stk.peek() != "("){
+                    String str = stk.pop();
+                    part.insert(0, str);
+                }
+                stk.pop(); //pop opening bracket as well
+                stk.push(part.reverse().toString());
+            }
+        }
+        
+        if(stk.size() > 1){
+            StringBuilder res = new StringBuilder("");
+            while(stk.size() > 0){
+                res.insert(0, stk.pop());
+            }
+            
+            return res.toString();
+        }
+        
+        return stk.peek();
+    }
+}
