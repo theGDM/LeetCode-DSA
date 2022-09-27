1a4803d4fd1a1c4507ad3c2e4997726fdd120ec7
theGDM <gyandeepmehra370@gmail.com>
2022-09-28 00:01:45 +0530

    Update 1190. Reverse Substrings Between Each Pair of Parentheses.java

--- Stack & Queue/1190. Reverse Substrings Between Each Pair of Parentheses.java
+++ Stack & Queue/1190. Reverse Substrings Between Each Pair of Parentheses.java
@@ -38,3 +38,48 @@ class Solution {
         return stk.peek();
     }
 }
+
+
+//"a(bcdefghijkl(mno)p)q"
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<StringBuilder> stk = new Stack<>();
+        StringBuilder open = new StringBuilder("("); //important
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                stk.push(open); //same address open bracket will be pushed in that..
+            }else if(ch >= 'a' && ch <= 'z'){
+                if(stk.size() > 0 && stk.peek() != open){
+                    stk.peek().append(ch);
+                }else{
+                    stk.push(new StringBuilder(ch + ""));
+                }   
+            }else{
+                StringBuilder part = new StringBuilder("");
+                while(stk.size() > 0 && stk.peek() != open){
+                    StringBuilder str = stk.pop();
+                    part.insert(0, str);
+                    
+                }
+                // System.out.println(part);
+                stk.pop(); //pop opening bracket as well
+                stk.push(part.reverse());
+            }
+            
+            System.out.println(stk);
+        }
+        
+        if(stk.size() > 1){
+            StringBuilder res = new StringBuilder("");
+            while(stk.size() > 0){
+                res.insert(0, stk.pop());
+            }
+            
+            return res.toString();
+        }
+        
+        return stk.peek().toString();
+    }
+}
