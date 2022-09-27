5683db017ca2dfaebe41d62d8c5f1740613a8e9b
theGDM <gyandeepmehra370@gmail.com>
2022-09-27 21:31:37 +0530

    Update 856. Score of Parentheses.java

--- Stack & Queue/856. Score of Parentheses.java
+++ Stack & Queue/856. Score of Parentheses.java
@@ -35,3 +35,37 @@ class Solution {
         }
     }
 }
+
+//TC : O(n);
+//SC : O(n);
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stk = new Stack<>();
+        
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                stk.push(-1); //( ==> corresponds to -1
+            }else{
+                int val = 0;
+                while(stk.peek() != -1){ //pop till we won't get the -1 i.e opening bracket..
+                    val += stk.pop();
+                }
+                stk.pop(); //remove the ( and push the double the val, if val != 0
+                if(val == 0) stk.push(1); //push () as 1
+                else stk.push(val * 2);
+            }
+        }
+        
+        if(stk.size() > 0){
+            int res = 0;
+            while(stk.size() > 0){
+                res += stk.pop();
+            }
+            return res;
+        }else{
+            return stk.peek();
+        }
+    }
+}
