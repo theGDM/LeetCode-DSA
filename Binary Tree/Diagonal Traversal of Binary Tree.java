d82ce1be9ddef3a2c299fd3b9d4cf4dc8b092e77
theGDM <gyandeepmehra370@gmail.com>
2022-10-02 19:11:44 +0530

    Create Diagonal Traversal of Binary Tree.java

---
+++ Binary Tree/Diagonal Traversal of Binary Tree.java
@@ -0,0 +1,110 @@
+/* Node is defined as
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}*/
+//BFS
+class Tree{
+      public class Pair{
+        Node node;
+        int dl;
+        
+        Pair(Node node, int dl){
+            this.node = node;
+            this.dl = dl;
+        }
+    }
+    
+    public ArrayList<Integer> diagonal(Node root) {
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(root, 0));
+
+        int maxdl = 0;
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        while(q.size() > 0){
+            int size = q.size();
+            while(size-- > 0){
+                Pair rem = q.remove(); //remove
+                
+                maxdl = Math.max(maxdl, rem.dl);
+                
+                if(hm.containsKey(rem.dl) == false){ //work2
+                    hm.put(rem.dl, new ArrayList<>());
+                }
+                hm.get(rem.dl).add(rem.node.data);
+                
+                if(rem.node.left != null){ //add left child of remove node
+                    q.add(new Pair(rem.node.left, rem.dl + 1)) ;
+                }
+                
+                if(rem.node.right != null){ //add right child of remove node
+                    q.add(new Pair(rem.node.right, rem.dl));
+                }
+            }   
+        }
+        
+        for(int i = 0; i <= maxdl; ++i){
+            ArrayList<Integer> part = hm.get(i);
+            for(int n : part){
+                res.add(n);
+            }
+        }
+        
+        return res;
+    }
+}
+
+/* Node is defined as
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}*/
+
+//TC : O(n) //DFS
+class Tree{
+    HashMap<Integer, ArrayList<Integer>> hm;
+    int maxdl;
+    public ArrayList<Integer> diagonal(Node root) {
+        hm = new HashMap<>();
+        maxdl = 0;
+        DFS(root, 0);
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i <= maxdl; ++i){
+            ArrayList<Integer> part = hm.get(i);
+            for(int n : part){
+                res.add(n);
+            }
+        }
+        
+        return res;
+    }
+    
+    public void DFS(Node node, int dl){
+        if(node == null) return;
+        
+        maxdl = Math.max(maxdl, dl); //updating for maximum diameter level
+        if(hm.containsKey(dl) == false){
+            hm.put(dl, new ArrayList<>());
+        }
+        hm.get(dl).add(node.data);
+        
+        DFS(node.left, dl + 1); //left child ko call karenge to diagonal level ek se badha denge
+        DFS(node.right, dl); //right child ko call karenge to digonal level same hoga
+    }
+}
+
