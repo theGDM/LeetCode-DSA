aa86e78e0a2a7c3ff06567bb1edb569b71906c0f
theGDM <gyandeepmehra370@gmail.com>
2022-10-04 12:28:15 +0530

    Update Inorder Successor in BST.java

--- Binary Search Tree/Inorder Successor in BST.java
+++ Binary Search Tree/Inorder Successor in BST.java
@@ -45,3 +45,32 @@ class Solution{
         return successor;
     }
 }
+
+//GFG Solution
+//TC : O(n), SC : O(1)
+class Solution{
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x){
+        Node successor = null;
+        if(x.right != null){ //if x uper hota hai to
+            successor = x.right;
+            while(successor.left != null){
+                successor = successor.left;
+            }
+            return successor;
+        }
+        
+        while(root != null){
+            if(x.data > root.data){
+                root = root.right;
+            }else if(x.data < root.data){
+                successor = root;
+                root = root.left;
+            }else{
+                break;
+            }
+        }
+        
+        return successor;
+    }
+}
