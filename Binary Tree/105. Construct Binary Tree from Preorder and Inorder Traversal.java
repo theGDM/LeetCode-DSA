38a1b88728b959859656159498297f412455c890
theGDM <gyandeepmehra370@gmail.com>
2022-10-04 17:44:30 +0530

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.java

--- Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -13,50 +13,6 @@
  *     }
  * }
  */
-//One Way, Without using extra space
-//More Than O(n) 
-//SC : O(1)
-class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode root = construct(0, preorder.length - 1, 0, inorder.length, preorder, inorder);
-        return root;
-    }
-    
-    public TreeNode construct(int pre_lo, int pre_hi, int in_lo, int in_hi, int[] pre, int[] in){
-        if(pre_lo > pre_hi || in_lo > in_hi){ //base case
-            return null;
-        }
-        
-        TreeNode root = new TreeNode(pre[pre_lo]);
-        int checkIdx = 0;
-        for(int i = 0; i < in.length; ++i){
-            if(pre[pre_lo] == in[i]){
-                checkIdx = i;
-            }
-        }
-        
-        int lenL = checkIdx - in_lo; //len on left side, after finding root in inorder array
-        root.left = construct(pre_lo + 1, pre_lo + lenL, in_lo, checkIdx - 1, pre, in);
-        root.right = construct(pre_lo + lenL + 1, pre_hi, checkIdx + 1, in_hi, pre, in);
-        return root;
-    }
-}
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 //TC : O(n);
 //SC : O(n)
 class Solution {
@@ -70,7 +26,7 @@ class Solution {
     }
     
     public TreeNode construct(int pre_lo, int pre_hi, int in_lo, int in_hi, int[] pre, HashMap<Integer, Integer> hm){
-        if(pre_lo > pre_hi){
+        if(pre_lo > pre_hi || in_lo > in_hi){
             return null;
         }
         
