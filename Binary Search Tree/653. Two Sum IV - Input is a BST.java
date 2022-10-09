c0348be49aa8f1744b5e51d188f53bf2ead48bbd
theGDM <gyandeepmehra370@gmail.com>
2022-10-09 10:21:28 +0530

    Update 653. Two Sum IV - Input is a BST.java

--- Binary Search Tree/653. Two Sum IV - Input is a BST.java
+++ Binary Search Tree/653. Two Sum IV - Input is a BST.java
@@ -46,3 +46,53 @@ class Solution {
         return false;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//TC : O(n), Using Inorder property of BST
+//SC : O(1), if we ignore the recursion call stack
+//We are given strictly increasing BST, so no chance for duplicate elements
+class Solution {
+    HashSet<Integer> set;
+    public boolean findTarget(TreeNode root, int k) {
+        set = new HashSet<>();
+        return helper(root, k);
+    }
+    
+    public boolean helper(TreeNode root, int k){
+        if(root == null) return false;
+        
+        boolean lans = helper(root.left, k);
+        if(lans){
+            return true;
+        }
+        
+        int remSum = k - root.val;
+        if(set.contains(remSum) == true){
+            return true;
+        }else{
+            set.add(root.val);
+        }    
+        
+        boolean rans = helper(root.right, k);
+        if(rans){
+            return true;
+        }
+        
+        return false;
+    }
+}
