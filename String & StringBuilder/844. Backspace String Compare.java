ffe3171fc8d3df33bdd3e9199df5828349dc4e1a
theGDM <gyandeepmehra370@gmail.com>
2022-10-15 19:02:18 +0530

    Update 844. Backspace String Compare.java

--- String & StringBuilder/844. Backspace String Compare.java
+++ String & StringBuilder/844. Backspace String Compare.java
@@ -23,3 +23,26 @@ class Solution {
         return ans.toString();
     }
 }
+
+//TC : O(s.length() + t.length())
+//SC : O(s.length() + t.length()), worst case, when no backslash charchter
+//using stack
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+    
+    public String build(String str){
+        Stack<Character> ans = new Stack<>();
+        for(int i = 0; i < str.length(); ++i){
+            char ch = str.charAt(i);
+            if(ch != '#'){
+                ans.push(ch);
+            }else if(ans.size() > 0){
+                ans.pop();
+            }
+        }
+        
+        return String.valueOf(ans);
+    }
+}
