a40235a02cc89039cdec1ff1d8e40390796f3fc9
theGDM <gyandeepmehra370@gmail.com>
2022-10-15 18:57:47 +0530

    Update 844. Backspace String Compare.java

--- String & StringBuilder/844. Backspace String Compare.java
+++ String & StringBuilder/844. Backspace String Compare.java
@@ -1,6 +1,5 @@
 //TC : O(s.length() + t.length())
 //SC : O(s.length() + t.length()), worst case, when no backslash charchter
-//2ms solution
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         return build(s).equals(build(t));
@@ -10,14 +9,14 @@ class Solution {
         StringBuilder ans = new StringBuilder();
         for(int i = 0; i < str.length(); ++i){
             char ch = str.charAt(i);
-            if(ch >= 'a' && ch <= 'z' && i + 1 < str.length() && str.charAt(i + 1) != '#'){
+            if(ch >= 'a' && ch <= 'z' && i + 1 < str.length() && str.charAt(i + 1) != '#'){  //"ab"
                 ans.append(ch);
-            }else if(ch >= 'a' && ch <= 'z' && i + 1 < str.length() && str.charAt(i + 1) == '#'){
-                i++;
+            }else if(ch >= 'a' && ch <= 'z' && i + 1 < str.length() && str.charAt(i + 1) == '#'){ //a#
+                i++; //not append that char, and also increment by 2
             }else if(ch == '#'){
                 if(ans.length() >= 1) ans.deleteCharAt(ans.length() - 1);
             }else{
-                ans.append(ch);
+                ans.append(ch); //last character
             }
         }
         
