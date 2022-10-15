0aeff3ff94ef2149d42b4f0e9291750916778f8b
theGDM <gyandeepmehra370@gmail.com>
2022-10-16 01:03:06 +0530

    Update 541. Reverse String II.java

--- String & StringBuilder/541. Reverse String II.java
+++ String & StringBuilder/541. Reverse String II.java
@@ -21,3 +21,31 @@ class Solution {
         return ans.toString();
     }
 }
+
+//TC : O(N)
+//SC : O(N)
+public class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int n = s.length();
+        
+        while(i < n){
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        
+        return new String(arr); //imp
+    }
+    
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
