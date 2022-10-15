cffad3b54ab490264bd75d46d9bd9e49a905c50d
theGDM <gyandeepmehra370@gmail.com>
2022-10-15 21:56:08 +0530

    Create 796. Rotate String.java

---
+++ String & StringBuilder/796. Rotate String.java
@@ -0,0 +1,29 @@
+//TC : O(N2);
+//SC : O(s.length)
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.equals(goal)) return true;
+        
+        char[] sarr = s.toCharArray();
+        for(int i = 1; i <= sarr.length; ++i){
+            String rotStr = rotateByX(sarr, i);
+            if(rotStr.equals(goal)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public String rotateByX(char[] arr, int x){ //O(n)
+        StringBuilder p1 = new StringBuilder();
+        StringBuilder p2 = new StringBuilder();
+        
+        for(int i = 0; i < arr.length; ++i){
+            if(i < x) p1.append(arr[i]);
+            else p2.append(arr[i]);
+        }
+        
+        return p2.append(p1).toString();
+    }
+}
