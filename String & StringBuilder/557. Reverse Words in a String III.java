0929fbaac89c9dedc9b58e798ac13b8e6bd22549
theGDM <gyandeepmehra370@gmail.com>
2022-10-16 01:19:31 +0530

    Update 557. Reverse Words in a String III.java

--- String & StringBuilder/557. Reverse Words in a String III.java
+++ String & StringBuilder/557. Reverse Words in a String III.java
@@ -26,3 +26,67 @@ class Solution {
         return s.toString();
     }    
 }
+
+//TC : O(n);
+//SC : O(n);
+class Solution {
+    public String reverseWords(String s) {
+        char[] chArr = s.toCharArray();
+        int l = 0, r = 0;
+        
+        while(r <= chArr.length){
+            if(r == chArr.length || chArr[r] == ' '){
+                swap(chArr, l, r - 1);
+                l = r; //moving left to where, right is currently 
+                l++; //moving left to 1 space ahead
+            }
+            r++;
+        }
+        
+        return new String(chArr);
+    }
+    
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
+
+//TC : O(n); //94 % faster
+//SC : O(n);
+class Solution {
+    public String reverseWords(String s) {
+        char[] chArr = s.toCharArray();
+        int l = 0, r = 0;
+        
+        while(r < chArr.length){
+            if(r == chArr.length - 1){
+                swap(chArr, l, r);
+                l = r; //moving left to where, right is currently 
+                l++;
+            }else if(chArr[r] == ' '){
+                swap(chArr, l, r - 1);
+                l = r; //moving left to where, right is currently
+                l++;
+            }
+            r++;
+        }
+        
+        return new String(chArr);
+    }
+    
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
