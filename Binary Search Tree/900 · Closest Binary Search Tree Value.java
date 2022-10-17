0dd568c9d6d3f7e4340b93e674ae23799786400a
theGDM <gyandeepmehra370@gmail.com>
2022-10-17 21:29:30 +0530

    Update 900 · Closest Binary Search Tree Value.java

--- Binary Search Tree/900 · Closest Binary Search Tree Value.java
+++ Binary Search Tree/900 · Closest Binary Search Tree Value.java
@@ -39,3 +39,46 @@ public class Solution {
         }
     }
 }
+
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+//TC : O(logn)
+//Without static variable
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @return: the value in the BST that is closest to the target
+     */
+    //fastest
+    //TC : O(logn)
+    public int closestValue(TreeNode root, double target) {
+        return (int)helper(root, target);
+    }
+
+    public static long helper(TreeNode root, double k){
+        if(root == null) return Long.MAX_VALUE;
+        
+        if(root.val > k){
+            long lans = helper(root.left, k);
+            if(Math.abs(lans - k) < Math.abs(root.val - k)) return lans;
+            else return root.val;
+        }else if(root.val < k){
+            long rans = helper(root.right, k);
+            if(Math.abs(rans - k) < Math.abs(root.val - k)) return rans;
+            else return root.val;
+        }else{
+            return root.val;
+        }
+    }
+}
