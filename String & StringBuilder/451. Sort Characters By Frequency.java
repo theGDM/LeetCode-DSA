192e7f38b5e278e2b65aa982d6bcfe88e2f29061
theGDM <gyandeepmehra370@gmail.com>
2022-10-19 15:07:18 +0530

    Update 451. Sort Characters By Frequency.java

--- String & StringBuilder/451. Sort Characters By Frequency.java
+++ String & StringBuilder/451. Sort Characters By Frequency.java
@@ -37,3 +37,41 @@ class Solution {
         return ans.toString();
     }
 }
+
+
+//TC : O(N)
+//Using Bucket sort technique
+public class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        //creating hashmap O(N)
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        } 
+        
+        //putting keys in buckets O(N)
+        List<Character>[] buckets = new List[s.length() + 1]; //because max frequency of any char is s.length()
+        for(char ch : map.keySet()){
+            int freq = map.get(ch);
+            if(buckets[freq] == null){
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(ch);
+        }
+        
+        //making the result
+        StringBuilder res = new StringBuilder();
+        for(int pos = buckets.length - 1; pos >= 0; --pos){
+            if(buckets[pos] != null){
+                for(char key : buckets[pos]){
+                    for(int i = 1; i <= pos; ++i){
+                        res.append(key);
+                    }
+                }
+            }
+        }
+        
+        return res.toString();
+    }
+}
