d23392fedbbc90b7b87fd020f0316a4f4d0a2095
theGDM <gyandeepmehra370@gmail.com>
2022-11-05 12:49:36 +0530

    Update 367. Valid Perfect Square.java

--- Binary Search/367. Valid Perfect Square.java
+++ Binary Search/367. Valid Perfect Square.java
@@ -41,3 +41,24 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long lo = 1;
+        long hi = num;
+        
+        while(lo <= hi){
+            long mid = lo + (hi - lo) / 2;
+            System.out.println(mid);
+            if(mid * mid == num){
+                return true;
+            }else if(mid * mid > num){
+                hi = mid - 1;
+            }else{
+                lo = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+}
