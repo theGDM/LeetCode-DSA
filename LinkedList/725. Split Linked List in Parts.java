8181ca0d66ff4f323b0aaf92b69523a62c9814d9
theGDM <gyandeepmehra370@gmail.com>
2022-11-11 19:01:48 +0530

    Update 725. Split Linked List in Parts.java

--- LinkedList/725. Split Linked List in Parts.java
+++ LinkedList/725. Split Linked List in Parts.java
@@ -1,3 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+//Create New List
+//TC : O(N + K) where NN is the number of nodes in the given list. If kk is large, it could still require creating many new empty lists.
+//SC : O(Max(N, k))
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] res = new ListNode[k];
+        int N = getSize(head);
+        
+        int width = N / k; //on avg there are N / k nodes in each part
+        int intialParts = N % k; //these first N % k parts have an extra item
+        
+        ListNode curr = head;
+        for(int i = 0; i < k; ++i){
+            ListNode dummy = new ListNode(-1);
+            ListNode dHead = dummy, dTail = dummy;
+            for(int j = 0; j < width + (i < intialParts ? 1 : 0); ++j){
+                ListNode temp = new ListNode(curr.val);
+                dTail.next = temp;
+                dTail = temp;
+                if(curr != null) curr = curr.next;
+            }
+            
+            res[i] = dHead.next;
+        }
+        
+        return res;
+    }
+    
+    public int getSize(ListNode head){
+        int size = 0;
+        while(head != null){
+            size++;
+            head = head.next;
+        }
+        
+        return size;
+    }
+}
+
 //Splitiing the Original List
 //TC : O(N + K) where NN is the number of nodes in the given list. If kk is large, it could still require creating many new empty lists.
 //SC : O(k)
