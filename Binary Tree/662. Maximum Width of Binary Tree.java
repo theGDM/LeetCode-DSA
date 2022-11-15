c5e40c8679665e1ae8af4cb30b4b2d904dab08bb
theGDM <gyandeepmehra370@gmail.com>
2022-11-15 16:45:56 +0530

    Update 662. Maximum Width of Binary Tree.java

--- Binary Tree/662. Maximum Width of Binary Tree.java
+++ Binary Tree/662. Maximum Width of Binary Tree.java
@@ -110,3 +110,64 @@ class Solution {
         return Math.max(left, right) + 1;
     }
 }
+
+
+//TC : O(N)
+//Level Order(More intuitive, without adding null node)
+class Solution {
+    public class Pair{
+        TreeNode node;
+        int idx;
+        
+        Pair(TreeNode node, int idx){
+            this.node = node;
+            this.idx = idx;
+        }
+    }
+    
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(root, 1));
+        
+        int level = 1;
+        int maxWidth = Integer.MIN_VALUE;
+        while(q.size() > 0){
+            int size = q.size();
+            int fIdx = 0; //first index
+            int sIdx = 0; //second index
+            boolean f = false; //boolean variable for setting sidx only once
+            while(size-- > 0){
+                Pair rem = q.remove();
+                // System.out.print(rem.idx + " ");
+                if(f == false && rem.node != null){ //setting first idx only one time
+                    fIdx = rem.idx;
+                    f = true;
+                }
+                
+                if(rem.node != null){//changing second index till last non-null node
+                    sIdx = rem.idx;
+                }
+                
+                if(rem.node.left != null){
+                    q.add(new Pair(rem.node.left, 2 * rem.idx));
+                }
+                
+                if(rem.node.right != null){
+                    q.add(new Pair(rem.node.right, 2 * rem.idx + 1));
+                }
+            }
+          
+            maxWidth = Math.max(maxWidth, sIdx - fIdx + 1);
+        }
+        
+        return maxWidth;
+    }
+    
+    public int getHeight(TreeNode root){
+        if(root == null) return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
