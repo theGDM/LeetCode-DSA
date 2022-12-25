a2aa5dbc77ffa3d0d769e9bac9c2c1f31b6b21b9
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2022-12-25 22:06:00 +0530

    Create 313. Super Ugly Number.java

---
+++ Dynamic Programming/313. Super Ugly Number.java
@@ -0,0 +1,26 @@
+//TC : O(N * Primes.length) 
+//SC : O(N) dp array + O(Primes.length) ptr array
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] ptr = new int[primes.length];
+        Arrays.fill(ptr, 1);
+        
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; ++i){
+            long min = Long.MAX_VALUE;
+            for(int j = 0; j < primes.length; ++j){
+                min = Math.min(min, dp[ptr[j]] * primes[j] * 1l);
+            }
+            
+            for(int j = 0; j < primes.length; ++j){
+                if(min == dp[ptr[j]] * primes[j] * 1l) ptr[j]++;
+            }
+            
+            dp[i] = min;
+        }
+        
+        return (int)dp[n];
+    }
+}
