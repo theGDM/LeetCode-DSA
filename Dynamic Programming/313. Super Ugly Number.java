8ecd03bf131056a4d83df9fd344f6247488442ee
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2022-12-25 22:16:54 +0530

    Update 313. Super Ugly Number.java

--- Dynamic Programming/313. Super Ugly Number.java
+++ Dynamic Programming/313. Super Ugly Number.java
@@ -1,4 +1,4 @@
-//TC : O(N * Primes.length) 
+//TC : O(N * (2 * Primes.length)) 
 //SC : O(N) dp array + O(Primes.length) ptr array
 class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
@@ -6,7 +6,7 @@ class Solution {
         Arrays.fill(ptr, 1);
         
         long[] dp = new long[n + 1];
-        dp[1] = 1;
+        dp[1] = 1;//add first ugly number at index 1
         
         for(int i = 2; i <= n; ++i){
             long min = Long.MAX_VALUE;
@@ -14,6 +14,7 @@ class Solution {
                 min = Math.min(min, dp[ptr[j]] * primes[j] * 1l);
             }
             
+            //updating all pointers having min value
             for(int j = 0; j < primes.length; ++j){
                 if(min == dp[ptr[j]] * primes[j] * 1l) ptr[j]++;
             }
