82526b3067971258f71375a69462472f057bc1a0
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2022-12-26 17:56:55 +0530

    Update 2389. Longest Subsequence With Limited Sum.java

--- Array/2389. Longest Subsequence With Limited Sum.java
+++ Array/2389. Longest Subsequence With Limited Sum.java
@@ -21,3 +21,41 @@ class Solution {
     }
 }
 
+//TC : O(nlogn + mlogn), where m is the size of queries
+//SC : O(n), prefix sum
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        //making the prefix sum array;
+        int[] preSum = new int[nums.length];
+        preSum[0] = nums[0]; 
+        for(int i = 1; i < nums.length; ++i){
+            preSum[i] = nums[i] + preSum[i - 1];
+        }
+        
+        int[] res = new int[queries.length];
+        int idx = 0;
+        for(int i = 0; i < queries.length; ++i){
+            int maxSum = queries[i];
+            
+            int low = 0, high = preSum.length - 1;
+            while(low < high){
+                int mid = low + (high - low) / 2;
+                
+                if(preSum[mid] >= maxSum){
+                    high = mid;
+                }else{
+                    low = mid + 1;
+                }
+            }
+             
+            if(preSum[low] > maxSum){
+                res[idx++] = low;
+            }else{
+                res[idx++] = low + 1;
+            }
+        }
+        
+        return res;
+    }
+}
