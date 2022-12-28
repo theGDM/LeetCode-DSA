228c292e522b13e5c98370b32a70a00c1fa89198
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2022-12-28 09:20:58 +0530

    Update 1696. Jump Game VI.java

--- Dynamic Programming/1696. Jump Game VI.java
+++ Dynamic Programming/1696. Jump Game VI.java
@@ -41,3 +41,42 @@ class Solution {
         return dp[idx] = ans + costs[idx];
     }
 }
+
+//TC : O(N * K), N is length of the nums array
+//SC : O(N)
+class Solution {
+    public static class Pair{
+        int min;
+        int max; 
+        Pair(int min, int max){
+            this.min = min;
+            this.max = max;
+        }
+    }
+    
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        return memo(0, n - 1, k, nums, dp);
+    }
+    
+    public int memo(int idx, int dest, int k, int[] costs, int[] dp){
+        if(idx == dest){
+            return dp[idx] = costs[idx];
+        }
+        
+        if(dp[idx] != Integer.MIN_VALUE){
+            return dp[idx];
+        }
+        
+        int ans = Integer.MIN_VALUE;
+        for(int jump = 1; jump <= k; ++jump){
+            if(idx + jump <= dest){
+                ans = Math.max(ans, memo(idx + jump, dest, k, costs, dp));
+            }
+        }
+        
+        return dp[idx] = ans + costs[idx];
+    }
+}
