8a8fa373d69e19489c4788812476e02a0e61752d
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2022-12-31 22:30:56 +0530

    Update 931. Minimum Falling Path Sum.java

--- Dynamic Programming/931. Minimum Falling Path Sum.java
+++ Dynamic Programming/931. Minimum Falling Path Sum.java
@@ -52,3 +52,72 @@ class Solution {
         return dp[r][c] = ans + matrix[r][c];
     }
 }
+
+//TC : O(n * n)
+//SC : O(n * n)
+//Tabulation
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int res = Integer.MAX_VALUE;
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for(int r = m - 1; r >= 0; --r){
+            for(int c = 0; c < n; ++c){
+                if(r == m - 1){
+                    dp[r][c] = matrix[r][c];
+                    continue;
+                }
+                
+                if(c == 0){
+                    dp[r][c] = Math.min(dp[r + 1][c], dp[r + 1][c + 1]) + matrix[r][c];
+                }else if(c == n - 1){
+                    dp[r][c] = Math.min(dp[r + 1][c - 1], dp[r + 1][c]) + matrix[r][c];
+                }else{
+                    dp[r][c] = Math.min(dp[r + 1][c - 1], Math.min(dp[r + 1][c], dp[r + 1][c + 1])) + matrix[r][c];
+                }
+            }
+        }
+        
+        //finding min in the first row of dp
+        for(int c = 0; c < n; ++c){
+            res = Math.min(res, dp[0][c]);
+        }
+        
+        return res;
+    }
+}
+
+//Space optimization
+//TC : O(n * n)
+//SC : O(n)
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int res = Integer.MAX_VALUE;
+        int m = matrix.length, n = matrix[0].length;
+        int[] dp = new int[n];
+        for(int c = 0; c < n; ++c){
+            dp[c] = matrix[m - 1][c];
+        }
+        
+        for(int r = m - 2; r >= 0; --r){
+            int[] tdp = new int[n];
+            for(int c = 0; c < n; ++c){
+                if(c == 0){
+                    tdp[c] = Math.min(dp[c], dp[c + 1]) + matrix[r][c];
+                }else if(c == n - 1){
+                    tdp[c] = Math.min(dp[c - 1], dp[c]) + matrix[r][c];
+                }else{
+                    tdp[c] = Math.min(dp[c - 1], Math.min(dp[c], dp[c + 1])) + matrix[r][c];
+                }
+            }
+            dp = tdp;
+        }
+        
+        //finding min in the first row of dp
+        for(int c = 0; c < n; ++c){
+            res = Math.min(res, dp[c]);
+        }
+        
+        return res;
+    }
+}
