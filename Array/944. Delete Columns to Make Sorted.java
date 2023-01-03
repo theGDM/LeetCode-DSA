6e8092888fd38b79ab393449f854d957e18b4eb8
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-03 09:50:18 +0530

    Update 944. Delete Columns to Make Sorted.java

--- Array/944. Delete Columns to Make Sorted.java
+++ Array/944. Delete Columns to Make Sorted.java
@@ -22,3 +22,20 @@ class Solution {
         return count;
     }
 }
+
+//TC : O(N * n), N is length of word, n is length of array
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        for(int i = 0; i < strs[0].length(); ++i){
+            for(int j = 0; j < strs.length - 1; ++j){ //O(n)
+                if(strs[j].charAt(i) > strs[j + 1].charAt(i)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
