d1814d4a8a541a6abbc245e606789a9875ead311
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-08 01:13:27 +0530

    Update 322. Coin Change.java

--- Dynamic Programming/322. Coin Change.java
+++ Dynamic Programming/322. Coin Change.java
@@ -15,3 +15,25 @@ class Solution {
         return count;
     }
 }
+
+//recursive code
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int ans = helper(amount, coins);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+    
+    public int helper(int amount, int[] denoms){
+        if(amount == 0) return 0;
+        
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < denoms.length; ++i){
+            if(amount - denoms[i] >= 0){
+                ans = Math.min(ans, helper(amount - denoms[i], denoms));
+            }
+        }
+        
+        if(ans != Integer.MAX_VALUE) return ans + 1;
+        else return ans;
+    }
+}
