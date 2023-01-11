8bff24eb4fc12babd89c51187c7d22979ad572e2
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-11 18:27:47 +0530

    Update Unbounded Knapsack(Knapsack with Duplicate Items).java

--- Dynamic Programming/Unbounded Knapsack(Knapsack with Duplicate Items).java
+++ Dynamic Programming/Unbounded Knapsack(Knapsack with Duplicate Items).java
@@ -24,3 +24,33 @@ class Solution{
         return dp[cap][idx] = Math.max(yes, no);
     }
 }
+
+//2nd approch
+//TC : O(items * capcity)
+//SC : O(items * capcity)
+class Solution{
+    static int knapSack(int N, int capacity, int costs[], int weight[]){
+        int[][] dp = new int[capacity + 1][costs.length + 1];
+        for(int i = 0; i < dp.length; ++i){
+            for(int j = 0; j < dp[0].length; ++j){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return memo(0, capacity, costs, weight, dp);
+    }
+    
+    public static int memo(int idx, int cap, int[] costs, int[] wt, int[][] dp){
+        if(idx == costs.length) return dp[cap][idx] = 0;
+        if(dp[cap][idx] != -1) return dp[cap][idx];
+        
+        int yes = -1;
+        if(cap - wt[idx] >= 0){
+            yes = memo(idx, cap - wt[idx], costs, wt, dp) + costs[idx];
+        }
+        
+        int no = memo(idx + 1, cap, costs, wt, dp);
+        
+        return dp[cap][idx] = Math.max(yes, no);
+    }
+}
