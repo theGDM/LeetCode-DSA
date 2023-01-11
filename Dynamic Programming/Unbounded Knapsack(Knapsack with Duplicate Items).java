f4abda5d766e631c7a288d16a46dadeaf8914582
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-11 22:59:28 +0530

    Update Unbounded Knapsack(Knapsack with Duplicate Items).java

--- Dynamic Programming/Unbounded Knapsack(Knapsack with Duplicate Items).java
+++ Dynamic Programming/Unbounded Knapsack(Knapsack with Duplicate Items).java
@@ -1,4 +1,4 @@
-//TC : O(items * capcity)
+//TC : O(items * capcity * capacity)
 //SC : O(items * capcity)
 class Solution{
     static int knapSack(int N, int capacity, int costs[], int weight[]){
@@ -52,5 +52,83 @@ class Solution{
         int no = memo(idx + 1, cap, costs, wt, dp);
         
         return dp[cap][idx] = Math.max(yes, no);
+    }
+}
+
+//Tabulation(Based on 1st Approach) cubic actually
+//TC : O(items * capcity * capacity)
+//SC : O(items * capcity)
+class Solution{
+    static int knapSack(int N, int capacity, int costs[], int weight[]){
+        int[][] dp = new int[costs.length + 1][capacity + 1];
+        for(int item = 0; item <= costs.length; ++item){
+            for(int cap = 0; cap <= capacity; ++cap){
+                if(item == 0 || cap == 0){
+                    dp[item][cap] = 0;
+                    continue;
+                }
+                
+                int yes = -1;
+                for(int freq = 1; cap >= weight[item - 1] * freq; ++freq){
+                    yes = Math.max(yes, dp[item - 1][cap - weight[item - 1] * freq] + costs[item - 1] * freq);
+                }
+                
+                int no = dp[item - 1][cap];
+                dp[item][cap] = Math.max(yes, no);
+            }
+        }
+        // for(int i = 0; i < dp.length; ++i){
+        //     System.out.println(Arrays.toString(dp[i]));
+        // }
+        return dp[costs.length][capacity];
+    }
+}
+
+//Tabulation(Based on 2nd Approach Optimized Calls) Quadratic actually
+class Solution{
+    static int knapSack(int N, int capacity, int costs[], int weight[]){
+        int[][] dp = new int[costs.length + 1][capacity + 1];
+        for(int item = 0; item <= costs.length; ++item){
+            for(int cap = 0; cap <= capacity; ++cap){
+                if(item == 0 || cap == 0){
+                    dp[item][cap] = 0;
+                    continue;
+                }
+                
+                int no = dp[item - 1][cap];
+                int yes = -1;
+                if(cap - weight[item - 1] >= 0){
+                    yes = dp[item][cap - weight[item - 1]] + costs[item - 1];
+                }
+                
+                dp[item][cap] = Math.max(yes, no);
+            }
+        }
+        // for(int i = 0; i < dp.length; ++i){
+        //     System.out.println(Arrays.toString(dp[i]));
+        // }
+        return dp[costs.length][capacity];
+    }
+}
+
+//Space Optimization
+//TC : O(items * capcity)
+//SC : O(capcity)
+class Solution{
+    static int knapSack(int N, int capacity, int costs[], int weight[]){
+        int[] dp = new int[capacity + 1];
+        for(int item = 1; item <= costs.length; ++item){
+            for(int cap = 1; cap <= capacity; ++cap){
+                
+                int no = dp[cap];
+                int yes = -1;
+                if(cap - weight[item - 1] >= 0){
+                    yes = dp[cap - weight[item - 1]] + costs[item - 1];
+                }
+                
+                dp[cap] = Math.max(yes, no);
+            }
+        }
+        return dp[capacity];
     }
 }
