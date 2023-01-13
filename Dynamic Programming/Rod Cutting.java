640b33ee86bde1754cd6dd10fe70a5e87ad6b5af
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-13 11:09:21 +0530

    Update Rod Cutting.java

--- Dynamic Programming/Rod Cutting.java
+++ Dynamic Programming/Rod Cutting.java
@@ -21,3 +21,22 @@ class Solution{
         return dp[len] = ans;
     }
 }
+
+//Tabulation
+//TC : O(N * N)
+//SC : O(N)
+class Solution{
+    public int cutRod(int price[], int n) {
+        int[] dp = new int[n + 1];
+        for(int len = 1; len <= n; ++len){
+            int max = -1;
+            for(int cutLen = 1; cutLen <= price.length; ++cutLen){
+                if(len - cutLen >= 0){
+                    max = Math.max(max, dp[len - cutLen] + price[cutLen - 1]) ;
+                }
+            }
+            dp[len] = max;
+        }
+        return dp[n];
+    }
+}
