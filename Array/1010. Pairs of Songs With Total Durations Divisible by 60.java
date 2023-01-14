aaf79fb88f9c982f7a3c6718a1bf0e3d01a871dc
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-14 22:18:05 +0530

    Update 1010. Pairs of Songs With Total Durations Divisible by 60.java

--- Array/1010. Pairs of Songs With Total Durations Divisible by 60.java
+++ Array/1010. Pairs of Songs With Total Durations Divisible by 60.java
@@ -1,7 +1,6 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         int[] fmap = new int[60];
-        
         int res = 0;
         for(int t : time){
             int val = t % 60;
