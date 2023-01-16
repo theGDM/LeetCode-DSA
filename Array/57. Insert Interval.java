f7426c2c91c86e280a2043c40ea0ebfe236cf907
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-01-16 19:34:46 +0530

    Update 57. Insert Interval.java

--- Array/57. Insert Interval.java
+++ Array/57. Insert Interval.java
@@ -28,3 +28,41 @@ class Solution {
         return res.toArray(new int[res.size()][2]);
     }
 }
+
+//TC : O(N)
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> list = new ArrayList<>();
+        //first step -> adding all the non overlapping intervals
+        
+        int i = 0;
+        while(i < intervals.length && intervals[i][1] < newInterval[0]){
+            list.add(intervals[i]);
+            i++;
+        }
+        
+        //step 2 -> making the merged Interval
+        int min = newInterval[0];
+        int max = newInterval[1];
+        while(i < intervals.length && intervals[i][0] <= newInterval[1]){
+            min = Math.min(min, intervals[i][0]);
+            max = Math.max(max, intervals[i][1]);
+            i++;
+        }
+        
+        list.add(new int[]{min, max});
+        
+        //third step - add remaining intervals
+        while(i < intervals.length){
+            list.add(intervals[i]);
+            i++;
+        }
+        
+        int[][] res = new int[list.size()][2];
+        for(i = 0; i < list.size(); ++i){
+            res[i] = list.get(i);
+        }
+        
+        return res;
+    }
+}
