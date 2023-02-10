1fec27a0d21a77622fe4c09fad227be92de80d92
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-02-11 00:45:27 +0530

    Update Print All Longest Increasing Subsequences(Any One).java

--- Dynamic Programming/Print All Longest Increasing Subsequences(Any One).java
+++ Dynamic Programming/Print All Longest Increasing Subsequences(Any One).java
@@ -1,3 +1,5 @@
+//TC : O(N * N * N)
+//SC : O(N * N)
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Queue;
@@ -36,6 +38,70 @@ public class Main{
         }
     }
    
+    public static void main(String []args){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = scn.nextInt();
+        }
+
+        solution(arr);
+        scn.close();
+    }
+}
+
+//Optimized approach
+//TC : O(N * N + N)
+//SC : O(N)
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main{
+    public static void solution(int[] nums){
+        int[] dp = new int[nums.length];
+        int[] prev = new int[nums.length];
+        int maxLIS = 0, maxLISidx = 0;
+        for(int i = 0; i < nums.length; ++i){ //o(N*N)
+            dp[i] = 1; //if prev does not exist, then current index can have LIS of length of 1
+            prev[i] = -1;
+            for(int j = 0; j < i; ++j){
+                if(nums[j] < nums[i]){
+                    if(dp[i] < dp[j] + 1){
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+            }
+            
+            if(maxLIS < dp[i]){
+                maxLIS = dp[i];
+                maxLISidx = i;
+            }
+        }
+        
+        //O(N)
+        ArrayList<Integer> LIS = new ArrayList<>();
+        while(maxLISidx != -1){
+            LIS.add(0, nums[maxLISidx]);
+            maxLISidx = prev[maxLISidx];
+        }
+        
+        System.out.println(maxLIS);
+        for(int i = 0; i < LIS.size(); ++i){
+            int val = LIS.get(i);
+            if(i == LIS.size() - 1){
+                System.out.print(val);
+            }else{
+                System.out.print(val + " -> ");
+            }
+        }
+    }
+ 
     public static void main(String []args){
         Scanner scn = new Scanner(System.in);
 
