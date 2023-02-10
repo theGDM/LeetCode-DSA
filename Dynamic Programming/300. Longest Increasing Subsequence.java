c20a27f0e769885a6ca7133d15da523afca24a75
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-02-10 20:22:18 +0530

    Update 300. Longest Increasing Subsequence.java

--- Dynamic Programming/300. Longest Increasing Subsequence.java
+++ Dynamic Programming/300. Longest Increasing Subsequence.java
@@ -1,33 +1,3 @@
-//LIS (Important category)
-//TC : O(n2)
-//arr[i] per khade hokar ye puchh rahe hai ki, arr[i], ko include karte hue maximum longest increasing 
-//subsequece kya hoga
-//jinke peeche lag sakta hai, unki lis value ka maximum liya, lis[i] me max + 1 add kar diya
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] lis = new int[nums.length];
-        lis[0] = 1;
-        
-        for(int i = 1; i < nums.length; ++i){
-            int max = 0;
-            for(int j = 0; j < i; ++j){
-                if(nums[j] < nums[i]){
-                    max = Math.max(max, lis[j]);
-                }
-            }
-            
-            lis[i] = max + 1;
-        }
-        
-        int res = 0;
-        for(int i = 0; i < lis.length; ++i){
-            res = Math.max(res, lis[i]);
-        }
-
-        return res;
-    }
-}
-
 //TC : O(2^N)
 class Solution {
     public int lengthOfLIS(int[] nums) {
@@ -46,4 +16,56 @@ class Solution {
         return Math.max(yes, no);
     }
     
+}
+
+//TC : O(N * N)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[][] dp = new int[nums.length + 1][nums.length + 1]; 
+        for(int i = 0; i < dp.length; ++i){
+            Arrays.fill(dp[i], -1);
+        }
+        
+        return memo(0, -1, nums, dp);
+    }
+    
+    public int memo(int curr, int prev, int[] nums, int[][] dp){
+        if(curr == nums.length) return 0;
+        
+        if(prev != -1 && dp[curr][prev] != -1) return dp[curr][prev];
+        
+        int yes = 0;
+        if(prev == -1 || nums[prev] < nums[curr]){
+            yes = memo(curr + 1, curr, nums, dp) + 1;
+        }
+        int no = memo(curr + 1, prev, nums, dp);
+        
+        if(prev != -1){
+            return dp[curr][prev] = Math.max(yes, no);
+        }
+        
+        return Math.max(yes, no);
+    }
+    
+}
+
+//TC : O(N * N)
+//SC : O(N)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int maxLIS = 0;
+        for(int i = 0; i < nums.length; ++i){
+            dp[i] = 1; //if prev does not exist, then current index can have LIS of length of 1
+            for(int j = 0; j < i; ++j){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            
+            maxLIS = Math.max(maxLIS, dp[i]);
+        }
+        
+        return maxLIS;
+    }
 }
