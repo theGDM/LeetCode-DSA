7f1b8795c25cb7a08be363a2217f520416918d08
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-02-12 00:38:32 +0530

    Update 674. Longest Continuous Increasing Subsequence.java

--- Dynamic Programming/674. Longest Continuous Increasing Subsequence.java
+++ Dynamic Programming/674. Longest Continuous Increasing Subsequence.java
@@ -15,3 +15,30 @@ class Solution {
         return max;
     }
 }
+
+//Printing
+//TC : O(N * N)
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int curr = 0, max = 0;
+        ArrayList<Integer> currList = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; ++i){
+            if(i > 0 && nums[i - 1] < nums[i]){
+                curr++;
+                currList.add(nums[i]);
+            }else{
+                curr = 1;
+                currList = new ArrayList<>();
+                currList.add(nums[i]);
+            }
+            
+            if(currList.size() > res.size()){
+                res = new ArrayList(currList);
+            }
+            max = Math.max(max, curr);
+        }
+        System.out.println(res);
+        return max;
+    }
+}
