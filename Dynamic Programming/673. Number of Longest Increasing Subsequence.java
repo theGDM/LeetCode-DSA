7158e2b214d757b7f1f7f08eda0e9703442801dd
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-02-11 22:39:49 +0530

    Update 673. Number of Longest Increasing Subsequence.java

--- Dynamic Programming/673. Number of Longest Increasing Subsequence.java
+++ Dynamic Programming/673. Number of Longest Increasing Subsequence.java
@@ -35,3 +35,40 @@ class Solution {
         return ans;
     }
 }
+
+
+//TC : O(N * N)
+//SC : O(2 * N)
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length]; //length of LIS ending at index i
+        Arrays.fill(dp, 1);
+        
+        int[] count = new int[nums.length]; //count of LIS ending at index i
+        Arrays.fill(count, 1);
+        
+        int maxLIS = 0;
+        for(int i = 0; i < nums.length; ++i){
+            for(int j = 0; j < i; ++j){
+                if(nums[j] < nums[i] && dp[i] <= dp[j] + 1){
+                    if(dp[i] < dp[j] + 1)
+                        count[i] = 0;
+                    
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    count[i] += count[j];
+                }
+            }
+                   
+            maxLIS = Math.max(maxLIS, dp[i]);
+        }
+        
+        int countLIS = 0;
+        for(int i = 0; i < dp.length; ++i){
+            if(dp[i] == maxLIS){ 
+                countLIS += count[i];
+            }
+        }
+
+        return countLIS;
+    }
+}
