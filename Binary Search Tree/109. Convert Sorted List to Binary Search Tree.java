a0a95b15e8a6d83275f2a1fa6a7fe757576421ab
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-03-13 15:16:59 +0530

    Update 109. Convert Sorted List to Binary Search Tree.java

--- Binary Search Tree/109. Convert Sorted List to Binary Search Tree.java
+++ Binary Search Tree/109. Convert Sorted List to Binary Search Tree.java
@@ -79,3 +79,73 @@ class Solution {
         return node;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//TC : O(nlog)
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        
+        if(head.next == null){
+            return new TreeNode(head.val);
+        }
+        
+        ListNode mid = getMid(head);
+        ListNode second = mid.next;
+        mid.next = null;
+        
+        //Now getting the list excluding the mid node
+        ListNode curr = head;
+        while(curr.next != mid){
+            curr = curr.next;
+        }
+        
+        curr.next = null;
+        TreeNode root = new TreeNode(mid.val);
+        TreeNode left = sortedListToBST(head);
+        TreeNode right = sortedListToBST(second);
+        
+        root.left = left;
+        root.right = right;
+        return root;
+        
+    }
+    
+    public ListNode getMid(ListNode head){
+        ListNode fast = head, slow = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
