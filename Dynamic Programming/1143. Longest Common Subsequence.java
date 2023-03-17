371045b4aa35cc6ded33ac7b1f5bfbb2df36a968
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-03-17 19:19:21 +0530

    Update 1143. Longest Common Subsequence.java

--- Dynamic Programming/1143. Longest Common Subsequence.java
+++ Dynamic Programming/1143. Longest Common Subsequence.java
@@ -73,3 +73,28 @@ class Solution {
         return dp[0][0];
     }
 }
+
+//TC : O(m * n)
+//SC : O(2 * m) - 2d dp
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[] next = new int[text2.length() + 1];
+        for(int i = text1.length() - 1; i >= 0; --i){
+            int[] curr = new int[text2.length() + 1];
+            for(int j = text2.length() - 1; j >= 0; --j){
+                int ch1 = text1.charAt(i);
+                int ch2 = text2.charAt(j);
+                
+                if(ch1 == ch2){
+                    curr[j] = 1 + next[j + 1];
+                }else{
+                    curr[j] = Math.max(next[j], curr[j + 1]);
+                }
+            }
+            
+            next = curr;
+        }
+        
+        return next[0];
+    }
+}
