9b13905ca1be54c4bbfc6cf6d020e6fc6e79499f
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-03-19 17:09:56 +0530

    Update 516. Longest Palindromic Subsequence.java

--- Dynamic Programming/516. Longest Palindromic Subsequence.java
+++ Dynamic Programming/516. Longest Palindromic Subsequence.java
@@ -71,10 +71,10 @@ class Solution {
         StringBuilder rev = new StringBuilder(s);
         rev = rev.reverse();
         String s2 = rev.toString();
-        return LIS(s, s2);
+        return LCS(s, s2);
     }
     
-    public int LIS(String s1, String s2){
+    public int LCS(String s1, String s2){
         int[][] dp = new int[s1.length() + 1][s2.length() + 2];
         for(int i = s1.length() - 1; i >= 0; --i){
             for(int j = s2.length() - 1; j >= 0; --j){
