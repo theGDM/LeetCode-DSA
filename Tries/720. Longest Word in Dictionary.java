d1e6576bf1c4da951defa7e3ba419d349bc8aa80
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-03-24 00:54:02 +0530

    Update 720. Longest Word in Dictionary.java

--- Tries/720. Longest Word in Dictionary.java
+++ Tries/720. Longest Word in Dictionary.java
@@ -1,3 +1,36 @@
+//Brute force using HM
+//TC : O(N * m), m is the length of the maximum size word, N is size of words array
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String str : words){
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        String ans = "";
+        for(int i = 0; i < words.length; ++i){
+            String word = words[i], curr = "";
+            for(int j = 0; j < word.length(); ++j){
+                String part = word.substring(0, j + 1);
+                if(map.containsKey(part)){
+                    curr = part;
+                }else{
+                    break;
+                }
+            }
+            
+            if(curr.length() > ans.length()){
+                ans = curr;
+            }
+        }
+        
+        return ans;
+    }
+}
+
+
+//Trie Solution
 class Solution {
     public static class Node{
         private Node[] children = new Node[26];
