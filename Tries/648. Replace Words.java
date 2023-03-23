b6bf00a1425fb4a6d970267c3b69664ae7966415
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-03-23 20:21:35 +0530

    Update 648. Replace Words.java

--- Tries/648. Replace Words.java
+++ Tries/648. Replace Words.java
@@ -82,3 +82,81 @@ class Solution {
         return res.substring(0, res.length() - 1);
     }
 }
+
+//Optimized solution
+class Solution {
+    public static class Node{
+        private Node[] children = new Node[26];
+        private boolean isEnd = false;
+        
+        public boolean contains(char ch){
+            return (children[ch - 'a'] != null) ? true : false;
+        }
+        
+        public Node get(char ch){
+            return children[ch - 'a'];
+        }
+        
+        public void set(char ch){
+            children[ch - 'a'] = new Node();
+        }
+        
+        public boolean getEnd(){
+            return isEnd;
+        }
+        
+        public void setEnd(){
+            isEnd = true;
+        }
+    }
+    
+    public void insert(Node curr, String word){
+        for(int i = 0; i < word.length(); ++i){
+            char ch = word.charAt(i);
+            if(curr.contains(ch) == false){
+                curr.set(ch);
+            }
+            
+            curr = curr.get(ch);
+        }
+        
+        curr.setEnd();
+    }
+     
+    public String search(Node curr, String word){
+        for(int i = 0; i < word.length(); ++i){
+            char ch = word.charAt(i);
+            
+            if(curr.getEnd() == true){
+                return word.substring(0, i);
+            }
+            
+            if(curr.contains(ch) == false){
+                return word;
+            }
+            
+            curr = curr.get(ch);
+        }
+        
+        return word;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Node root = new Node();
+        for(int i = 0; i < dictionary.size(); ++i){
+            String word = dictionary.get(i);
+            insert(root, word);
+        }
+        
+        String[] words = sentence.split(" ");
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < words.length; ++i){
+            String word = words[i];
+            String prefix = search(root, word);
+            res.append(prefix);
+            res.append(" ");
+        }
+        
+        return res.toString().substring(0, res.length() - 1);
+    }
+}
